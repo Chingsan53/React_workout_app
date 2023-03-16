99300cb041435fe61cefe1e549219639da977bc0
Chingsan53 <89380886+Chingsan53@users.noreply.github.com>
2023-03-15 22:58:39 -0700

    update hook

--- src/App.js
+++ src/App.js
@@ -1,57 +1,109 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 
 import SearchBox from './components/search-box/search-box.component';
 import CardList from './components/card-list/card-list.component';
 import './App.css';
 
-class App extends Component {
-  constructor() {
-    super();
+const App = () => {
+  const[searchField, setSearchField] = useState(''); // [value, setValue]
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilterMonsters] = useState(monsters);
+  const [stringField, setStringField] = useState();
 
-    this.state = {
-      monsters: [],
-      searchField: ''
-    };
-  }
+  console.log('render');
 
-  componentDidMount() {
-    console.log('componentDidMount')
+  useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
-      .then((response) => response.json())
-      .then((users) => this.setState(() => {
-        return { monsters: users };
-      }
-      )
-      );
+    .then((response) => response.json())
+    .then((users) => setMonsters(users));
+  }, []);
 
-  }
-
-  onSearchChange = (event) => {
-    const searchField = event.target.value.toLocaleLowerCase();
-    this.setState(() => {
-      return { searchField };
-    });
-  };
-
-  render() {
-    const { monsters, searchField } = this.state;
-    const { onSearchChange } = this;
-
-    const filteredMonsters = monsters.filter((monster) => {
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
       return monster.name.toLocaleLowerCase().includes(searchField);
     });
 
-    return (
-      <div className='App'>
+    setFilterMonsters(newFilteredMonsters);
+
+    console.log('effect is firing')
+  }, [monsters, searchField]);
+
+  const onSearchChange = (event) => {
+        const searchFieldString = event.target.value.toLocaleLowerCase();
+        setSearchField(searchFieldString);
+  };
+
+  const onStringChange = (event) => {
+    setStringField(event.target.value);
+  }
+
+
+  return (
+    <div className='App'>
+        <h1 className='text-7xl bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 font-bigelow-rules'>Monster Rolodex</h1>
         <SearchBox
           className='search-box'
           onChangeHandler={onSearchChange}
           placeholder='search monsters'
         />
+        <SearchBox
+          onChangeHandler={onStringChange}
+          placeholder='set string'
+        />
         <CardList monsters={filteredMonsters} />
       </div>
-    );
-  }
-}
+  );
+};
+
+// class App extends Component {
+//   constructor() {
+//     super();
+
+//     this.state = {
+//       monsters: [],
+//       searchField: ''
+//     };
+//   }
+
+//   componentDidMount() {
+//     console.log('componentDidMount')
+//     fetch('https://jsonplaceholder.typicode.com/users')
+//       .then((response) => response.json())
+//       .then((users) => this.setState(() => {
+//         return { monsters: users };
+//       }
+//       )
+//       );
+
+//   }
+
+//   onSearchChange = (event) => {
+//     const searchField = event.target.value.toLocaleLowerCase();
+//     this.setState(() => {
+//       return { searchField };
+//     });
+//   };
+
+//   render() {
+//     const { monsters, searchField } = this.state;
+//     const { onSearchChange } = this;
+
+//     const filteredMonsters = monsters.filter((monster) => {
+//       return monster.name.toLocaleLowerCase().includes(searchField);
+//     });
+
+//     return (
+//       <div className='App'>
+//         <h1 className='text-7xl bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 font-bigelow-rules'>Monster Rolodex</h1>
+//         <SearchBox
+//           className='search-box'
+//           onChangeHandler={onSearchChange}
+//           placeholder='search monsters'
+//         />
+//         <CardList monsters={filteredMonsters} />
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'bigelow-rules': ['"Bigelow Rules"', 'cursive'],
+      },
+    },
   },
   plugins: [],
 }
